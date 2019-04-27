8c80bd14346d0a575b8b26c79ec25ec6ab2afc93
bro-wer <bronek.werla@gmail.com>
2019-04-27 20:41:34 +0200

    Added args validation

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import sys
 
+def ValidateArgsCount():
+    if len(sys.argv) != 2:
+        raise Exception("Unexpected args count!")
+
 if __name__ == '__main__':
     try:
-        print("Args count = " + str(len(sys.argv)))
+        ValidateArgsCount()
+        print(str(len(sys.argv[1])))
     except Exception as e:
         print(e)
